cc6ae0983c09f03aba4cd4e0c81856105f0eee2f
Yuri Zmytrakov <yuri.zmytrakov@gmail.com>
2025-07-11 18:33:03 +0200

    Set up urls

--- remarcable/urls.py
+++ remarcable/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.product_list, name='product_list'),
+    path('products/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
